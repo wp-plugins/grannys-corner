6f36eeba6654026dff71dbe63ac70500d2921c8c
opajaap <opajaap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-04 14:04:33 +0000

    Initial release
    git-svn-id: https://plugins.svn.wordpress.org/grannys-corner/trunk@328608 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ grannys-corner.php
@@ -0,0 +1,120 @@
+<?php
+/*
+Plugin Name: Grannys corner
+Description: Text widget for Granny's column
+Version: 1.0
+Author: J.N. Breetvelt a.k.a OpaJaap
+Author URI: http://www.opajaap.nl/
+Plugin URI: http://wordpress.org/extend/plugins/grannys-corner
+*/
+
+load_plugin_textdomain('grc', 'wp-content/plugins/grannys-corner/langs/', 'grannys-corner/langs/');
+/**
+ * GrannysCorner Class
+ */
+class GrannysCorner extends WP_Widget {
+    /** constructor */
+    function GrannysCorner() {
+        parent::WP_Widget(false, $name = 'Granny\'s Corner Widget');	
+		$widget_ops = array('classname' => 'widget_grannys_corner', 'description' => __( 'Granny\'s column text widget', 'grc') );	//
+		$this->WP_Widget('grannys_corner', __('Granny\'s Corner', 'grc'), $widget_ops);															//
+    }
+
+	/** @see WP_Widget::widget */
+    function widget($args, $instance) {		
+		global $wpdb;
+		global $widget_content;
+
+        extract( $args );
+        
+ 		$title = apply_filters('widget_title', empty( $instance['title'] ) ? __( 'Granny\'s Corner', 'grc' ) : $instance['title']);
+		if (is_user_logged_in()) {
+			$itsme = $this->get_user() == $instance['user'];
+		}
+		else $itsme = false;
+
+		$updated = false;
+		if ($itsme) {
+			if (isset($_POST['grc-'.$instance['user']])) {
+				update_option('grannys-corner-'.$instance['user'], htmlspecialchars($_POST['grc-'.$instance['user']]));
+				$updated = true;
+			}
+		}
+
+		$the_text = stripslashes(get_option('grannys-corner-'.$instance['user']));
+		
+		if ($itsme && !$updated) {
+			$widget_content = '<form action="'.get_option('siteurl').'" method="post" >';
+			$widget_content .= '<textarea name="grc-'.$instance['user'].'" style="width:95%; height:250px;">'.$the_text.'</textarea>';
+			$widget_content .= '<input type="submit" class="button-primary" name="grc-submit" value="'.__('Save Changes', 'grc').'" />';
+			$widget_content .= '</form>';
+		}
+		else {
+			$widget_content = get_option('grannys-corner-'.$instance['user']);
+		}
+
+		echo $before_widget . $before_title . $title . $after_title . $widget_content . $after_widget;
+    }
+	
+
+    /** @see WP_Widget::update */
+    function update($new_instance, $old_instance) {				
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['user'] = $new_instance['user'];
+
+        return $instance;
+    }
+
+    /** @see WP_Widget::form */
+    function form($instance) {				
+		//Defaults
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'user' => '') );
+		$title = esc_attr( $instance['title'] );
+		$user = $instance['user'];
+?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'grc'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('user'); ?>"><?php _e( 'User:', 'grc' ); ?></label>	
+			<select name="<?php echo $this->get_field_name('user'); ?>" id="<?php echo $this->get_field_id('user'); ?>" class="widefat">
+<?php
+			$this->user_select();
+?>
+			</select>
+		</p>
+<?php
+    }
+
+	function get_user() {
+	global $current_user;
+		get_currentuserinfo();
+		$user = $current_user->user_login;
+		return $user;
+	}
+
+	function get_users() {
+	global $wpdb;
+		$users = $wpdb->get_results('SELECT * FROM '.$wpdb->users, 'ARRAY_A');
+		return $users;
+	}
+
+	function user_select($select = '') {
+		$result = '';
+		$iam = $select == '' ? $this->get_user() : $select;
+		$users = $this->get_users();
+		foreach ($users as $usr) {
+			if ($usr['user_login'] == $iam) $sel = 'selected="selected"';
+			else $sel = '';
+			$result .= '<option value="'.$usr['user_login'].'" '.$sel.'>'.$usr['display_name'].'</option>';
+		}	
+		echo ($result);
+	}
+	
+} // class GrannysCorner
+
+// register GrannysCorner widget
+add_action('widgets_init', create_function('', 'return register_widget("GrannysCorner");'));
+?>
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Grannys Corner ===
+Contributors: opajaap
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=OpaJaap@OpaJaap.nl&item_name=Grannys-Corner&item_number=Support-Open-Source&currency_code=USD&lc=US
+Tags: textwidget, widget
+Version: 1.0
+Stable tag: trunk
+Author: J.N. Breetvelt
+Author URI: http://www.opajaap.nl/
+Requires at least: 2.8
+Tested up to: 3.0.4
+
+This plugin is designed to easily enter a text in a widget for a specific user without the need to enter the admin screen.
+
+
+== Description ==
+
+This plugin is designed to easily enter a text in a widget for a specific user without the need to enter the admin screen.
+
+At activation of a Grannys Corner widget, a title can be entered and a username selected. 
+When the user that 'owns' the widget is logged in, the text area is editable for him/her.
+When he presses the save button, he will see the final result. 
+If he/she wants to change the content, the page needs to be re-opened by the logged in user.
+Multiple instances of the widget can be activated and owned by different users.
+The plugin can be translated. Dutch language files are included.
+
+== Installation ==
+
+You can install the plugin from the WP Admin Plugins page, or manually.
+
+== Frequently Asked Questions ==
+
+= How can i translate the plugin into my language? =
+
+* Find on internet the free program POEDIT, and learn how it works.
+* Use the file grc.pot that is located in grannys-corner/langs to create or update
+grc-[your languagecode].po and grc-[your languagecode].mo.
+* Place these file in the langs subdir.
+* If everything is ok, mail me the files and i will distribute them so other users can use it too.
+* For more information on POT files, domains, gettext and i18n have a look at the I18n for 
+WordPress developers Codex page and more specifically at the section about themes and plugins.
+
+== Changelog ==
+
+= 1.0 = 
+
+* 1.0 Initial release
+
+== Upgrade Notice ==
+
+== Licence ==
+
+Grannys Corner is released under the GNU GPL licence. ( http://www.gnu.org/copyleft/gpl.html )
